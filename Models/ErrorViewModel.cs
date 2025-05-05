8fb13304847894dd27da59fdad7189f141eada7e
mariamelshahat <mm6002@fayoum.edu.eg>
2025-05-05 17:23:34 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SearchEngine.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
