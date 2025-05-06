56dee62d25d26e62ee3a54cbab7700b6e71ff7d5
mariamelshahat <mm6002@fayoum.edu.eg>
2025-05-06 17:41:22 +0300

    .

--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseRouting();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Search}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}");
 
 app.Run();
